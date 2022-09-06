3f6fd9c8ed47cd633b57b5d491254d3abefd7ea0
qzy <1103111833@qq.com>
2022-09-06 11:06:15 +0800

    测试

--- PrismDemo/PrismDemo/obj/Debug/net6.0-windows/Views/Page1.g.i.cs
+++ PrismDemo/PrismDemo/obj/Debug/net6.0-windows/Views/Page1.g.i.cs
@@ -105,7 +105,7 @@ namespace PrismDemo.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PrismDemo;component/views/page1.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PrismDemo;V1.0.0.0;component/views/page1.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\Page1.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
