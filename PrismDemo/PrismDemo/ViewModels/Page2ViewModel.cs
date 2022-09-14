e68413f92a17e979457a8c0b641ae340d3ee8563
null <1103111833@qq.com>
2022-09-14 20:32:38 +0800

    测试freesql导航属性

--- PrismDemo/PrismDemo/ViewModels/Page2ViewModel.cs
+++ PrismDemo/PrismDemo/ViewModels/Page2ViewModel.cs
@@ -11,6 +11,8 @@ using PrismDemo.Models;
 using System.Runtime.Intrinsics.X86;
 using System.Diagnostics;
 using FreeSql;
+using System.Threading;
+using System.Security.Cryptography.Xml;
 
 namespace PrismDemo.ViewModels
 {
@@ -26,7 +28,7 @@ namespace PrismDemo.ViewModels
                 SetProperty(ref name, value);
             }
         }
-        public List<Task> TaskList { get; set; }
+        public List<Topic> TaskList { get; set; }
         public Page2ViewModel(IContainerExtension container)
         {
             List<Task> list = new List<Task>();
@@ -34,8 +36,45 @@ namespace PrismDemo.ViewModels
             list.Add(new Task { Id = 11, Name = "任务11", Status = 0, Category = null, CategoryId = 0 });
             list.Add(new Task { Id = 12, Name = "任务12", Status = 0, Category = null, CategoryId = 0 });
             freeSql = container.Resolve<Init>().GetInstance();
+           
             //TaskList = freeSql.Select<Task>().ToList();
-            TaskList = freeSql.Select<Task>().IncludeMany(a=>a.Category.Where(b=>b.Id==a.Id)).ToList();
+            //freeSql.Insert(list).ExecuteAffrows();
+            freeSql.Aop.CurdAfter += (s, e) =>
+            {
+                if (e.ElapsedMilliseconds > 200)
+                {
+                    //记录日志
+                    //发送短信给负责人
+                }
+            };
+            freeSql.GlobalFilter.Apply<Task>("filter", a => a.Id == 1);
+            //TaskList = freeSql.Select<Task>().IncludeMany(a=>a.Category.Where(b=>b.Id==a.Id)).ToList();
+
+            List<Topic> topic_l = new List<Topic>()
+            {
+                new Topic{Title="topic1",CreateTime=DateTime.Now,Clicks=10,Category=new Category{Id=1, Name="category1",Parent=new CategoryType{ Id=1,Name="categorytype1"} },CategoryId=1 },
+                new Topic{Title="topic2",CreateTime=DateTime.Now,Clicks=11,Category=new Category{Id=2, Name="category2",Parent=new CategoryType{ Id=2,Name="categorytype2"} },CategoryId=2 },
+                new Topic{Title="topic3",CreateTime=DateTime.Now,Clicks=12,Category=new Category{Id=3, Name="category3",Parent=new CategoryType{ Id=3,Name="categorytype3"} },CategoryId=3 }
+            };
+            List<CategoryType> ctype_l = new List<CategoryType>
+            {
+                new CategoryType{ Id=1,Name="categorytype1"},
+                new CategoryType{ Id=2,Name="categorytype2"},
+                new CategoryType{ Id=3,Name="categorytype3"}
+            };
+            List<Category> cat_l = new List<Category>
+            {
+               new Category{Id=1, Name="category1",Parent=new CategoryType{ Id=1,Name="categorytype1"} },
+               new Category{Id=2, Name="category2",Parent=new CategoryType{ Id=2,Name="categorytype2"} },
+               new Category{Id=3, Name="category3",Parent=new CategoryType{ Id=3,Name="categorytype3"} }
+            };
+            //freeSql.Insert(ctype_l).ExecuteAffrows();
+            //freeSql.Insert(cat_l).ExecuteAffrows();
+            TaskList = freeSql.Select<Topic>()
+  .LeftJoin(a => a.Category.Id == a.CategoryId)
+  //.LeftJoin(a => a.Category.Parent.Id == a.Category.ParentId)
+  .Where(a => a.Category.Parent.Id > 0)
+  .ToList();
         }
     }
 }
